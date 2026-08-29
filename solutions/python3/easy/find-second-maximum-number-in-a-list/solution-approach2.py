d3e85980526c51bcfa284dea7b5aa2a46c658347
Krishna Choudhary <krishnachoudhary1633304@gmail.com>
2026-08-29 23:52:23 +0530

    ✅ Find the Runner-Up Score!   [python3] — approach2

---
+++ solutions/python3/easy/find-second-maximum-number-in-a-list/solution-approach2.py
@@ -0,0 +1,16 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?isFullScreen=true
+# Problem     Find the Runner-Up Score!  
+# Difficulty  Easy
+# Subdomain   Basic Data Types
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-08-29, 11:52 p.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(set([int(x) for x in (input().split())]))
+    arr.sort()
+    print(arr[-2])
