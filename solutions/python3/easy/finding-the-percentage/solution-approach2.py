1a8c45b60df987e8326968618e989904955af695
Krishna Choudhary <krishnachoudhary1633304@gmail.com>
2026-09-02 20:31:19 +0530

    ✅ Finding the percentage [python3] — approach2

---
+++ solutions/python3/easy/finding-the-percentage/solution-approach2.py
@@ -0,0 +1,21 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/finding-the-percentage/problem?isFullScreen=true
+# Problem     Finding the percentage
+# Difficulty  Easy
+# Subdomain   Basic Data Types
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-02, 08:31 p.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    avg = (sum(student_marks[query_name]))/len(student_marks[query_name])
+    print(f"{avg:.2f}")
