345e6436aff6bbd3fe7e13a137c55760f75ee314
Krishna Choudhary <krishnachoudhary1633304@gmail.com>
2026-09-03 11:50:21 +0530

    ✅ sWAP cASE [python3] — approach1

---
+++ solutions/python3/easy/swap-case/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/swap-case/problem?isFullScreen=true
+# Problem     sWAP cASE
+# Difficulty  Easy
+# Subdomain   Strings
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-03, 11:50 a.m.
+# ──────────────────────────────────────────────────
+
+def swap_case(s):
+    s_new = s.swapcase()
+    return s_new
+
