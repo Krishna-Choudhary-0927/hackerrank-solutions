d2155bd2cc40558408ed2fe48e3bf27d603c9c32
Krishna Choudhary <krishnachoudhary1633304@gmail.com>
2026-09-08 19:21:40 +0530

    ✅ String Split and Join [python3] — approach1

---
+++ solutions/python3/easy/python-string-split-and-join/solution-approach1.py
@@ -0,0 +1,16 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+# Problem     String Split and Join
+# Difficulty  Easy
+# Subdomain   Strings
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-08, 07:21 p.m.
+# ──────────────────────────────────────────────────
+
+def split_and_join(line):
+    line = line.split()
+    line = "-".join(line)
+    return line
+
